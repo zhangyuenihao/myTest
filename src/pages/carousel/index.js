789da2e28615d3a7a1cf530570729bbdb937e620
zhangyuenihao <1914759018@qq.com>
2019-06-03 01:59:01 +0800

    1.做qq列表切换，for嵌套for 2.轮播图添加缩略图和小圆点

---
+++ src/pages/carousel/index.js
@@ -0,0 +1,60 @@
+import './index.scss'
+
+window.onload = function () {
+    var imgIcon = document.getElementsByClassName('img-icon')
+    var pre = document.getElementsByClassName('pre')
+    var next = document.getElementsByClassName('next')
+    var circleList = document.getElementsByClassName('circle-list')
+    var circle = circleList[0].getElementsByClassName('circle')
+    var thumbnail = document.getElementsByClassName('thumbnail')
+    var arrUrl = ['/images/carousel/11.jpg', '/images/carousel/12.jpg', '/images/carousel/13.jpg', '/images/carousel/14.jpg']
+    var oldCircle = null
+    var num = 0
+
+    for (var i = 0; i < arrUrl.length; i++) {
+        circleList[0].innerHTML += '<li class="circle"><div class="thumbnail"><img src="' + arrUrl[i] + '"class="thum - icon"></div></li>'
+
+    }
+    oldCircle = circle[num]
+
+    function init(num) {
+        oldCircle.className = 'circle'
+        circle[num].className += ' active'
+        imgIcon[0].src = arrUrl[num]
+        oldCircle = circle[num]
+    }
+
+    init(num)
+
+    pre[0].onclick = function () {
+        num--
+        if (num < 0) {
+            num = arrUrl.length - 1
+        }
+        init(num)
+    }
+
+    next[0].onclick = function () {
+        num++
+        if (num > arrUrl.length - 1) {
+            num = 0
+        }
+        init(num)
+    }
+    for (var i = 0; i < circle.length; i++) {
+        circle[i].index = i
+        circle[i].onclick = function () {
+            num = this.index
+            init(num)
+        }
+        circle[i].onmouseover = function () {
+            num = this.index
+            thumbnail[num].style.display = 'block'
+        }
+        circle[i].onmouseout = function () {
+            num = this.index
+            thumbnail[num].style.display = 'none'
+        }
+    }
+
+}
---
+++ src/pages/qq/index.js
@@ -0,0 +1,41 @@
+import './index.scss'
+
+window.onload = function () {
+    var item = document.getElementsByClassName('item')
+    var title = document.getElementsByClassName('title')
+    var itemList = document.getElementsByClassName('item-list')
+    var user = null
+    var arrUser = []
+    var num = 0
+    var num2 = 0
+    var oldtitle = null
+    var olduser = null
+
+    for (var i = 0; i < title.length; i++) {
+        title[i].index = i
+        title[i].onclick = function () {
+            oldtitle = title[num]
+            oldtitle.className = 'title'
+            itemList[num].style.display = 'none'
+            this.className += ' active'
+            itemList[this.index].style.display = 'block'
+            num = this.index
+        }
+    }
+    for (var i = 0; i < item.length; i++) {
+        user = item[i].getElementsByClassName('user')
+        for (var j = 0; j < user.length; j++) {
+            arrUser.push(user[j])
+        }
+    }
+
+    for (var i = 0; i < arrUser.length; i++) {
+        arrUser[i].index = i
+        arrUser[i].onclick = function () {
+            olduser = arrUser[num2]
+            olduser.className = 'user'
+            this.className += ' active'
+            num2 = this.index
+        }
+    }
+}
